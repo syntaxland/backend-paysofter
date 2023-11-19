3b0e91c1acbbc6c20ca7a5abfe93ee08655af577
syntaxland <chibuzo.okenwa@email.com>
2023-11-19 12:01:55 +0100

    new update

--- promise/tasks.py
+++ promise/tasks.py
@@ -4,6 +4,7 @@ import string
 
 # from celery import Celery
 from celery import shared_task
+from decimal import Decimal
 from django.db.models import Sum
 
 from promise.models import PaysofterPromise
@@ -20,34 +21,81 @@ def process_promise_transactions():
 
     # Query promises that are fulfilled by buyers
     fulfilled_promises = PaysofterPromise.objects.filter(buyer_promise_fulfilled=True, is_delivered=False)
-
     print(f"\nFulfilled promises count: {len(fulfilled_promises)}")
 
     if fulfilled_promises:
         for promise in fulfilled_promises:
+            # Deduct 2% if is_settle_conflict_activated is True
+            amount = promise.amount
+            if promise.is_settle_conflict_activated:
+                amount *= 0.98  # Deduct 2%
+
             # Create a Transaction record for each fulfilled promise
             transaction = Transaction.objects.create(
                 seller=promise.seller,
                 buyer_email=promise.buyer.email,
-                amount=promise.amount,
+                amount=amount,
                 currency=promise.currency,
                 payment_method=promise.payment_method,
                 is_approved=True,
-                payout_status=False,  # Assuming payout is done immediately after transaction
+                payout_status=False,  
                 is_success=promise.is_success,
-                payment_id=promise.promise_id,  # Assuming promise_id can be used as payment_id
-                transaction_id=generate_transaction_id(),  # You may generate a unique transaction_id
+                payment_id=promise.promise_id,  
+                transaction_id=generate_transaction_id(),  
                 payment_provider=promise.payment_provider,
             )
             print(f"\nThe transaction created: {transaction}")
 
         fulfilled_promises.update(is_delivered=True)
 
-
-
         # transfer funds to sellers
 
     return f"{len(fulfilled_promises)} promise transactions processed successfully"
+
+
+# @shared_task
+# def process_promise_transactions():
+#     print("\nPromise transactions processing started!")
+
+#     # Query promises that are fulfilled by buyers
+#     fulfilled_promises = PaysofterPromise.objects.filter(buyer_promise_fulfilled=True, is_delivered=False)
+#     print(f"\nFulfilled promises count: {len(fulfilled_promises)}")
+
+    
+
+#     # # promise_amount_after_charges = Decimal(str(charges_amount)) * Decimal('0.02')
+
+#     # if fulfilled_promises.is_settle_conflict_activated == True:
+#     #     promise_amount = fulfilled_promises.amount
+#     #     charges_percentage = Decimal('0.02')
+#     #     charges_amount = promise_amount * charges_percentage
+#     #     promise_amount_after_charges = promise_amount - charges_amount
+
+#     if fulfilled_promises:
+#         for promise in fulfilled_promises:
+#             # Create a Transaction record for each fulfilled promise
+#             transaction = Transaction.objects.create(
+#                 seller=promise.seller,
+#                 buyer_email=promise.buyer.email,
+#                 amount=promise.amount,
+#                 currency=promise.currency,
+#                 payment_method=promise.payment_method,
+#                 is_approved=True,
+#                 payout_status=False,  
+#                 is_success=promise.is_success,
+#                 payment_id=promise.promise_id,  
+#                 transaction_id=generate_transaction_id(),  
+#                 payment_provider=promise.payment_provider,
+#             )
+#             print(f"\nThe transaction created: {transaction}")
+
+        
+
+#         fulfilled_promises.update(is_delivered=True)
+
+#         # transfer funds to sellers
+
+#     return f"{len(fulfilled_promises)} promise transactions processed successfully"
 
 
 
