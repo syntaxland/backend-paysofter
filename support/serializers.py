726eda304aa9666cbccfef8bb987f5e72503d95e
syntaxland <chibuzo.okenwa@email.com>
2024-02-12 09:15:32 +0100

    new update

--- support/serializers.py
+++ support/serializers.py
@@ -2,9 +2,12 @@
 from rest_framework import serializers
 from .models import SupportTicket, SupportResponse
 
-
+     
 class SupportTicketSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    account_id = serializers.CharField(source='user.account_id', read_only=True)
     first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
     email = serializers.CharField(source='user.email', read_only=True)
 
     class Meta: 
@@ -13,7 +16,10 @@ class SupportTicketSerializer(serializers.ModelSerializer):
 
  
 class SupportResponseSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    account_id = serializers.CharField(source='user.account_id', read_only=True)
     first_name = serializers.CharField(source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
     email = serializers.CharField(source='user.email', read_only=True)
 
     class Meta:
